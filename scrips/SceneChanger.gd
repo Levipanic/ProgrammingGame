ed3b08dfaac831eb3bf6834393bf64a84681a94e
413 <59661037+Levipanic@users.noreply.github.com>
2023-02-27 18:56:46 +0300

    Delete scrips directory

--- scrips/SceneChanger.gd
+++
@@ -1,4 +0,0 @@
-extends CanvasLayer
-
-func change_scene(path):
-	get_tree().change_scene(path)
--- scrips/scenebutton.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-
-public class scenebutton : Button
-{
- [Export]
- private string sceneTOopen = "";
-}
